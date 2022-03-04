f4358b5b59eb28024e3d169d36ba7258675a371c
Trevor Hobenshield <59624054+trevorhobenshield@users.noreply.github.com>
2022-03-03 19:01:31 -0800

    Update nn.py

--- nn.py
+++ nn.py
@@ -3,11 +3,8 @@ from icecream import ic,argumentToString
 from numpy import nan_to_num
 from numpy.random import permutation,normal
 
-
 @argumentToString.register(np.ndarray)
-def _(arr):
-    return f"{arr}\tshape: {arr.shape}"
-
+def _(a):return f"{a}\t{a.shape = }"
 
 class MLP:
     def __init__(self,L):
@@ -17,7 +14,7 @@ class MLP:
         self.f,self.df = lambda x:1./(1.+np.exp(-x)),lambda x:self.f(x)*(1.-self.f(x))
         self.C,self.dC = lambda a,y:np.sum(nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a))),lambda a,y:a-y
         self.Z,self.A = ...,...
-        self.zero_grads = lambda:([np.zeros_like(w) for w in self.W],[np.zeros_like(b) for b in self.b])
+        self.zero_grads = lambda:([np.zeros_like(w)for w in self.W],[np.zeros_like(b)for b in self.b])
 
     def SGD(self,train,epochs=3,sz=32,lr=0.01,reg=0.5):
         """ Mini-Batch Stochastic Gradient Descent """
@@ -25,7 +22,7 @@ class MLP:
         for epoch in range(epochs):
             train = permutation(train)
             ic(epoch)
-            for i,mb in enumerate(train[n:n+sz] for n in range(0,m,sz)):
+            for i,mb in enumerate(train[n:n+sz]for n in range(0,m,sz)):
                 gW,gb = self.zero_grads()
                 mb_loss = 0.
                 for j,(X,y) in enumerate(mb):
@@ -34,7 +31,7 @@ class MLP:
                     gW = np.sum([gW,dgW],axis=0)
                     gb = np.sum([gb,dgb],axis=0)
                 self.W = [(1-lr*(reg/m))*W-(lr/len(mb))*gW for W,gW in zip(self.W,gW)]
-                self.b = [b-((lr*gb)/len(mb)) for b,gb in zip(self.b,gb)]
+                self.b = [b-((lr*gb)/len(mb))for b,gb in zip(self.b,gb)]
                 mb_loss += .5*(reg/len(train))*sum(np.linalg.norm(w)**2 for w in self.W)
                 if i%200==199:ic(mb_loss)
 
